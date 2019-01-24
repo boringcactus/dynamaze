fa786236ab34003975a536d23a615a8cbd4ef8c1
Melody Horn <melody@boringcactus.com>
2019-01-23 17:03:30 -0700

    don't deploy to itch.io on branches or pull requests

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cargo build --release
+mkdir dist
+cp -r assets dist/
+if [[ "$TRAVIS_OS_NAME" = "windows" ]]; then
+    SUFFIX=.exe
+else
+    SUFFIX=
+fi
+cp target/release/dynamaze$SUFFIX dist/
+if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+    BUTLER_DIST=darwin
+else
+    BUTLER_DIST=$TRAVIS_OS_NAME
+fi
+curl -L -o butler.zip https://broth.itch.ovh/butler/$BUTLER_DIST-amd64/LATEST/archive/default
+unzip butler.zip
+chmod +x butler$SUFFIX
+./butler$SUFFIX -V
+./butler$SUFFIX push dist boringcactus/dynamaze:$TRAVIS_OS_NAME
